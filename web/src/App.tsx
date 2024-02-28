6713541ed72ddf976149ea716a331f05d94a0723
Dries Verboven <verboven.dries@gmail.com>
2024-02-28 20:35:11 +0100

    adding functioning tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,11 @@
-import { useEffect } from 'react'
-import { EmojiRow } from './components/emojiRow/emojiRow'
 import { StartView } from './views/start/start.view'
-import { useEmojiSelect } from './utils/hooks/openEmoji'
 
+import './index.css'
+ 
 function App() {
-  const { emojis, loading, fetchEmojis } = useEmojiSelect()
-
-  useEffect(() => {
-    fetchEmojis(5)
-  }, [])
-
-  if (loading) <div>Loading</div>
-  if (emojis?.length === 0) <div>ERROR</div>
-
   return (
     <>
-      <EmojiRow emojis={emojis!} />
-      <button onClick={() => fetchEmojis(5)}>renew</button>
+      <h1 className="text-6xl text-sky-500">test</h1>
       <StartView />
     </>
   )
--- web/src/views/start/start.view.tsx
+++ web/src/views/start/start.view.tsx
@@ -1,7 +1,24 @@
+import { useEffect } from 'react'
+import { useEmojiSelect } from '#/utils/hooks/openEmoji'
+import { EmojiRow } from '#/components/emojiRow/emojiRow'
+
 export const StartView = () => {
+  const { emojis, loading, fetchEmojis } = useEmojiSelect()
+
+  useEffect(() => {
+    fetchEmojis(5)
+  }, [])
+
+  if (loading) <div>Loading</div>
+  if (emojis?.length === 0) <div>ERROR</div>
+
   return (
-    <div>
-      <p>hello</p>
-    </div>
+    <main className="container mx-auto">
+      <nav>
+        <h1 className="text-3xl">ATOFE</h1>
+      </nav>
+      <EmojiRow emojis={emojis!} />
+      <button onClick={() => fetchEmojis(5)}>renew</button>
+    </main>
   )
 }
