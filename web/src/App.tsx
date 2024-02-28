23e088c50f816b3046994376dc5786692490d663
Dries Verboven <verboven.dries@gmail.com>
2024-02-28 14:09:50 +0100

    implment new emoji hook

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,23 @@
-import { EmojiRow } from "./components/emojiRow/emojiRow"
-import { emojiTestList } from "./testData/emojis"
-import { StartView } from "./views/start/start.view"
-
-
-
+import { useEffect } from 'react'
+import { EmojiRow } from './components/emojiRow/emojiRow'
+import { StartView } from './views/start/start.view'
+import { useEmojiSelect } from './utils/hooks/openEmoji'
 
 function App() {
+  const { emojis, loading, fetchEmojis } = useEmojiSelect()
+
+  useEffect(() => {
+    fetchEmojis(5)
+  }, [])
+
+  if (loading) <div>Loading</div>
+  if (emojis?.length === 0) <div>ERROR</div>
 
   return (
     <>
-      <EmojiRow emojis={emojiTestList}/>
+      <EmojiRow emojis={emojis!} />
+      <button onClick={() => fetchEmojis(5)}>renew</button>
       <StartView />
-      <div>Hello</div>
     </>
   )
 }
