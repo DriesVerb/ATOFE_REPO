aca9ecbcbf89a50f7231345174794a34049e9993
Dries Verboven <verboven.dries@gmail.com>
2024-03-04 21:01:46 +0100

    adding responsivness to main container

--- web/src/elements/layout/container/container.tsx
+++ web/src/elements/layout/container/container.tsx
@@ -4,7 +4,7 @@ import { Route, Switch } from 'wouter'
 export const MainContainer = () => {
 
   return (
-    <main className="p-2 lg:max-w-screen-2xl grid grid-cols-12 auto-rows-auto">
+    <main className="p-2 gap-2 md:gap-3 lg:max-w-screen-2xl grid grid-cols-12 auto-rows-auto">
       <Switch>
         <BaseRoute />
         <Route>404</Route>
