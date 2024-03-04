8ad54069c1aaf9581b3f05a3d393b5fa1719d3ae
Dries Verboven <verboven.dries@gmail.com>
2024-03-04 10:56:19 +0100

    move text to header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
+import { Txt } from './elements'
 import { MainContainer } from './elements/layout/container/container'
 
 function App() {
   return (
     <main className="container mx-auto">
-      <header>Header</header>
+      <header>
+      <Txt.H1 text="ATOFE" className="flex justify-center"/>
+      <Txt.H2 text="slogan heyoo" className="flex justify-center"/>
+      <Txt.H3 text="a tale of two emojis" className="flex justify-center"/>
+      </header>
       <MainContainer />
       <footer>footer</footer>
     </main>
--- web/src/views/start/start.view.tsx
+++ web/src/views/start/start.view.tsx
@@ -11,9 +11,6 @@ export const StartView = () => {
 
   return (
     <>
-      <Txt.H1 text="ATOFE" className="col-span-12 flex justify-center"/>
-      <Txt.H2 text="slogan heyoo" className="flex justify-center col-span-12"/>
-      <Txt.H3 text="a tale of two emojis" className="flex justify-center col-span-12"/>
       <Txt.p className="col-span-4">Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</Txt.p>
       <div className="flex gap-4 justify-end">
         <Avatar />
