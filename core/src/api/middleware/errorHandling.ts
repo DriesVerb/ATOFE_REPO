32362003f88cc952aa808de4bee4af6410b597e5
Dries Verboven <verboven.dries@gmail.com>
2024-03-12 20:26:22 +0100

    better logging

--- core/src/api/middleware/errorHandling.ts
+++ core/src/api/middleware/errorHandling.ts
@@ -2,7 +2,6 @@ import { ErrorRequestHandler } from 'express'
 import { logger } from '../../utils/logger'
 
 export const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
-  console.log("next" + error)
-  logger.error(error.message)
+  logger.error(`[Error Handerling request]: ${error.message}`)
   res.status(500).json({error: error.message }).send()
 }
