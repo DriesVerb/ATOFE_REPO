9c8c51f7adf6731810628ac87f1e8cf6f31ee3fc
Dries Verboven <verboven.dries@gmail.com>
2024-04-03 15:01:10 +0200

    change logic of react hook

--- web/src/api/user/hooks/mutate/loginUser.tsx
+++ web/src/api/user/hooks/mutate/loginUser.tsx
@@ -2,18 +2,19 @@ import { Login } from '#/types/auth'
 import { useMutation } from '@tanstack/react-query'
 import { loginUser } from '../../func/login'
 import { AxiosError } from 'axios'
-import { navigate } from 'wouter/use-browser-location'
-import { createLoginUser } from '#/utils/library/loggedInUser'
+import { useLocalUser } from '#/utils/library/loggedInUser'
 
 export const useLoginUser = (href: string = '/') => {
+
+  const {setUser} = useLocalUser(href)
+
   return useMutation({
     mutationFn: (body: Login) => loginUser(body),
     onError: (error: AxiosError) => {
       console.log(error)
     },
     onSuccess: (result) => {
-      createLoginUser(result.data)
-      navigate(href)
+      setUser(result.data)
     },
   })
 }
