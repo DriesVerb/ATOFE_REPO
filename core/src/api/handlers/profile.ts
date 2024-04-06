bf884d31b9d9c7471f1c250b1cd951883cb2d29d
Dries Verboven <verboven.dries@gmail.com>
2024-04-06 21:49:11 +0200

    adding profile create/update

--- core/src/api/handlers/profile.ts
+++ core/src/api/handlers/profile.ts
@@ -1,16 +1,22 @@
-import { RequestHandler } from "express"
-import { logger } from "../../utils/logger"
-import { reqHan } from "../../utils/consts/loggingMessage"
-import { meProfile } from "../../services/profile/profile"
+import { RequestHandler } from 'express'
+import { logger } from '../../utils/logger'
+import { reqHan } from '../../utils/consts/loggingMessage'
+import { createProfile, meProfile, updateProfile } from '../../services/profile/profile'
 
 export const me: RequestHandler = async (req, res) => {
-  logger.info(reqHan("User profile"))
+  logger.info(reqHan('User profile'))
   const result = await meProfile(req.body)
   res.send(result)
 }
 
+export const create: RequestHandler = async (req, res) => {
+  logger.info(reqHan('create profile'))
+  const result = await createProfile(req.body)
+  res.send(result)
+}
 
-export const edit: RequestHandler = async (_req, res) => {
-  logger.info(reqHan("Edit profile"))
-  res.send("heelo")
+export const update: RequestHandler = async (req, res) => {
+  logger.info(reqHan('update profile'))
+  const result = await updateProfile(req.body)
+  res.send(result)
 }
--- core/src/services/profile/profile.ts
+++ core/src/services/profile/profile.ts
@@ -1,16 +1,59 @@
-import { getPrisma } from "../../databases/prisma"
+import { getPrisma } from '../../databases/prisma'
 
 export const meProfile = async (body: any) => {
   const prisma = getPrisma()
 
-  console.log(body.token)
-
-
   const profile = await prisma.profile.findUnique({
     where: {
-      userId: body.token.userId
-   }
+      userId: body.token.userId,
+    },
   })
 
-  return {profile}
+  return { profile }
+}
+
+export const createProfile = async (body: any) => {
+  const prisma = getPrisma()
+
+  const token = body.token
+
+  const { bio, avatar, avatarBg } = body
+
+  try {
+    await prisma.profile.create({
+      data: {
+        bio: bio,
+        avatar: avatar,
+        avatarBg: avatarBg,
+        userId: token.userId,
+      },
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  return { message: 'created profile' }
+}
+
+export const updateProfile = async (body: any) => {
+  const prisma = getPrisma()
+
+  const { profileId, bio, avatar, avatarBg } = body
+
+  try {
+    await prisma.profile.update({
+      where: {
+        id: profileId
+      },
+      data: {
+        bio: bio,
+        avatar: avatar,
+        avatarBg: avatarBg,
+      },
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  return { message: 'created profile' }
 }
