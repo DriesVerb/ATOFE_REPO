dc6106dda74924763b78d1cde17c26ecebbc6dfc
Dries Verboven <verboven.dries@gmail.com>
2024-04-26 21:13:54 +0200

    adding emojis to story creator

--- web/src/views/story/storyCreate.tsx
+++ web/src/views/story/storyCreate.tsx
@@ -1,5 +1,7 @@
 import { Editor } from '#/components/editor/editor'
+import { EmojiRow } from '#/components/emojiRow/emojiRow'
 import { Txt } from '#/elements'
+import { useEmojiStore } from '#/store/emoji'
 
 const story = {
   type: 'doc',
@@ -59,9 +61,12 @@ const story = {
 }
 
 export const StoryCreate = () => {
+  const emojis = useEmojiStore((state) => state.emojis)
+
   return (
     <>
-        <Txt.H2 ngclass="col-span-1 md:col-span-5"  text="Title" />
+      <Txt.H2 ngclass="col-span-1 md:col-span-5" text="Title" />
+      <EmojiRow emojis={emojis} />
       <div className="col-span-1 md:col-span-5">
         <Editor story={story} />
       </div>
